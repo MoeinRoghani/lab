6efdd036ddfa87b5a7fb4006ead02dba23b839cc
Moein <62990666+MoeinRoghani@users.noreply.github.com>
2023-01-18 14:09:29 -0500

    Update blocked.py

--- blocked.py
+++ blocked.py
@@ -1,22 +1,3 @@
-import mysql.connector
-import re
-import requests
-#from bs4 import BeautifulSoup
-#https://regex101.com/
-
-print("\nConnecting to DATABASE...\n")
-config = {
-  'user': 'root',
-  'password': '#Private#SQL',
-  'host': '127.0.0.1',
-  'database': 'ssh_blocked',
-  'raise_on_warnings': True
-}
-
-cnx = mysql.connector.connect(**config)
-cursor = cnx.cursor()
-print("--------------------------Connection Stablished--------------------------\n")
-
 
 def ip_user():
 	ip_user = input("Enter the IPv4: ")
